660e7df1933b5c69a23c6adb433963279b5d19d5
gilbert <gilbert.bageni@student.moringaschool.com>
2023-08-21 14:59:59 +0300

    passed

--- lib/dog.py
+++ lib/dog.py
@@ -2,6 +2,16 @@
 
 class Dog:
     # Class body goes here
+    def bark(self):
+        print("Woof!")
+    def sit(self):
+        print("The dog is sitting.")
 
-    #Instance method definition
-    pass
+# Creating a Dog object
+simba=Dog()
+simba = Dog()
+
+# Calling the instance method
+simba.bark()
+simba.sit()
+pass
--- lib/person.py
+++ lib/person.py
@@ -2,6 +2,15 @@
 
 class Person:
     # Class body goes here
+      def talk(self):
+            print ("Hello World!")
+      def walk(self):
+            print ("The person is walking.")
+            
+ #creating a person object
+gilbert = Person()
 
-    #Instance method definition
-    pass
+#Instance method definition
+gilbert.talk()
+gilbert.walk()
+pass
